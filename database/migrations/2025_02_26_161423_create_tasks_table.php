64e69a7ad26b92986c349bf3faeee728f3e0d15f
Mo.Araiqat <107495343+mohammad-ariqat@users.noreply.github.com>
2025-02-26 19:36:29 +0300

    Created the tables(task , catagory)

--- database/migrations/2025_02_26_161423_create_tasks_table.php
+++ database/migrations/2025_02_26_161423_create_tasks_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');//on delete cascade means that if the user is deleted, the tasks will be deleted
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();//null on delete means that if the category is deleted, the task will not be deleted but the category_id will be set to null
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('due_date')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');
             $table->timestamps();
         });
     }
